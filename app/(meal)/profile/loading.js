ad028b7fb156737ad3e40216ad4c049ce70163f7
Chamod <webdevprocm@gmail.com>
2024-10-03 19:01:56 +0530

    added loading.js file in each route

--- app/(meal)/profile/loading.js
+++ app/(meal)/profile/loading.js
@@ -4,7 +4,7 @@ export default function LoadingPage(){
     return(
         <main className="max-w-[1400px] h-screen flex justify-center items-center mx-auto">
             <CgSearchLoading size={50}/>
-            <h1 className="sm:text-5xl text-4xl text-gray-100 font-bold uppercase">Loading your page...</h1>
+            <h1 className="sm:text-5xl text-4xl text-gray-100 font-bold uppercase">Loading your Profile...</h1>
         </main>
     )
 }
